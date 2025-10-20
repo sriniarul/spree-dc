a444b6555a614e0831c175aa243215ce1c03dd35
Arulsrinivaasan <sriniarul444@gmail.com>
2025-10-20 09:54:51 +0530

    push notification

--- core/config/initializers/devise_oauth.rb
+++ core/config/initializers/devise_oauth.rb
@@ -14,13 +14,30 @@ if defined?(Devise)
                         access_type: 'online',
                         name: 'google_oauth2'
                       }
+    elsif ENV['GOOGLE_OAUTH_CLIENT_ID'] && ENV['GOOGLE_OAUTH_CLIENT_SECRET']
+      config.omniauth :google_oauth2,
+                      ENV['GOOGLE_OAUTH_CLIENT_ID'],
+                      ENV['GOOGLE_OAUTH_CLIENT_SECRET'],
+                      {
+                        scope: 'email,profile',
+                        prompt: 'consent',
+                        image_aspect_ratio: 'square',
+                        image_size: 50,
+                        access_type: 'online',
+                        name: 'google_oauth2'
+                      }
     end
   end
+end
 
-  # Add omniauthable module to devise configuration
-  if Spree.user_class.respond_to?(:devise_modules)
-    unless Spree.user_class.devise_modules.include?(:omniauthable)
-      Spree.user_class.devise :omniauthable, omniauth_providers: [:google_oauth2]
+# Configure User model for OAuth after initialization
+Rails.application.config.after_initialize do
+  if defined?(Devise) && defined?(Spree) && Spree.respond_to?(:user_class)
+    user_class = Spree.user_class
+    if user_class.respond_to?(:devise_modules) && user_class.devise_modules.present?
+      unless user_class.devise_modules.include?(:omniauthable)
+        user_class.devise :omniauthable, omniauth_providers: [:google_oauth2]
+      end
     end
   end
 end
