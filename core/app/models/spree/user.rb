9e0ac7133bf56864849f424fe068ddfa859a2cc1
Arulsrinivaasan <sriniarul444@gmail.com>
2025-10-20 14:21:17 +0530

    remove omni auth

--- core/app/models/spree/user.rb
+++ core/app/models/spree/user.rb
@@ -1,24 +1,14 @@
 class Spree::User < Spree.base_class
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :omniauthable,
-         omniauth_providers: [:google_oauth2]
+         :recoverable, :rememberable, :validatable
 
   include Spree::UserAddress
   include Spree::UserMethods
   include Spree::UserPaymentSource
-  include Spree::UserOauth if defined?(Spree::UserOauth)
 
   validates :email, presence: true, uniqueness: { scope: spree_base_uniqueness_scope }
 
   before_validation :set_login
-
-  def self.from_omniauth(auth)
-    where(email: auth.info.email).first_or_create do |user|
-      user.email = auth.info.email
-      user.password = Devise.friendly_token[0, 20]
-      user.login = auth.info.email
-    end
-  end
 
   private
 
